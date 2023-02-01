f899db6a1d1ea2eb2d31b679dcdaec4c9f227711
Bakai0 <ulanbekovbakai00@gmail.com>
2023-02-01 15:40:32 +0300

    mmmmmm

---
+++ app/src/main/java/com/example/trynavigation/ui/Onclick.kt
@@ -0,0 +1,7 @@
+package com.example.trynavigation.ui
+
+import com.example.trynavigation.ui.data.FirstModel
+
+interface Onclick {
+    fun onClick(model: FirstModel)
+}
---
+++ app/src/main/java/com/example/trynavigation/ui/repozitory/SecondRepozitory.kt
@@ -0,0 +1,101 @@
+package com.example.trynavigation.ui.repozitory
+
+import com.example.trynavigation.ui.data.FirstModel
+
+class SecondRepozitory {
+    private var textList2 = mutableListOf<FirstModel>()
+
+    fun getListOfText2(): MutableList<FirstModel> {
+        textList2.add(
+            FirstModel(
+                "https://avatars.mds.yandex.net/i?id=8e085fbb11af85e07032b52f44f45" +
+                        "8fd2537e812-5087064-images-thumbs&n=13", "Continue"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://media.kg-portal.ru/images/ninjaassassin/ninjaassassin" +
+                        "_22.jpg", "Switching Protocols"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://kinoscan.com/wp-content/uploads/2022/03/" +
+                        "luchshie-filmyi-v-retsenziyah-nindzya-ninja-2009-1.jpg", "Processing"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://scottadkinsworld.com/wp-content/up" +
+                        "loads/2021/05/Ninja-Scott-Adkins-20.jpg", "Early Hints"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://avatars.mds.yandex.net/get-kinopoisk-image/16006" +
+                        "47/e8b757b2-3971-4f49-b2ae-2366a10a040e/3840x", "OK"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://media.filmz.ru/photos/full/filmz.ru_" +
+                        "" +
+                        "f_23966.jpg", "Created"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "http://m.kino-teatr.ru/movie/kadr/155721/11618" +
+                        "" +
+                        "35.jpg", "Accepted"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://www.aceshowbiz.com/images/still/ninja-" +
+                        "" +
+                        "shadow-of-a-tear-img08.jpg", "Non-Authoritative Information"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://media.filmz.ru/photos/full/filmz.ru" +
+                        "_f_32672.jpg", "No Content"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://mobimg.b-cdn.net/v3/fetch/a8/a86c88c" +
+                        "" +
+                        "3ae9e819daf5406b65fd2ddd2.jpeg", "Partial Content"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://i.pinimg.com/originals/e0/83/af/e083afb96e08ebf" +
+                        "8f04257c62ae941d9.jpg", "Multi-Status"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://avatars.mds.yandex.net/get-kinopoisk-image/1900" +
+                        "788/ca25d33a-2b08-4d48-98d8-cdcc2b5749bf/3840x", "Multiple Choices"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://www.aceshowbiz.com/images/still/ninja-shadow-" +
+                        "of-a-tear08.jpg", "Moved Permanently"
+            )
+        )
+        textList2.add(
+            FirstModel(
+                "https://media.kg-portal.ru/images/ninjaassassin/ninj" +
+                        "aassassin_13.jpg", "Found"
+            )
+        )
+
+        return textList2
+    }
+}
+
